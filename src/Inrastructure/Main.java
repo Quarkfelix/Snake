6807acca759017ea7c1a5cf1212287d602f92932
marc <marc@pop-os.localdomain>
2020-07-03 21:12:40 +0200

    penis music

--- src/Inrastructure/Main.java
+++ src/Inrastructure/Main.java
@@ -1,12 +1,14 @@
 package Inrastructure;
 
 import Draw.GUI;
+import libary_version_2.MusicPlayer;
 
 public class Main {
 	public static GUI gui;
 	public static MyWindow window;
 	
 	public static void main(String[] args) {
+		MusicPlayer mp = new MusicPlayer("drawings/gmbs.wav");
 		window = new MyWindow();
 		gui = new GUI();
 		
